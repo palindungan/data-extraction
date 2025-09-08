916a4ee793bd77dfb748052085467fb8f940e93e
palindungan <rizkika.palindungan1@gmail.com>
2025-09-08 23:53:14 +0700

    Update mining_news.py

--- scripts/repositories/mining_news.py
+++ scripts/repositories/mining_news.py
@@ -52,6 +52,49 @@ class MiningNews:
         return result
 
     @staticmethod
+    def update(
+            mining_source_id=None,
+            code=None,
+            data=None,
+    ):
+        import os
+        import json
+        import psycopg2
+        from psycopg2 import extras
+        from dotenv import load_dotenv
+
+        load_dotenv()
+        db_url = os.getenv("DATABASE_URL")
+        connection = psycopg2.connect(db_url)
+
+        if data:
+            data = json.dumps(data)
+
+        with connection:
+            with connection.cursor(cursor_factory=extras.DictCursor) as cursor:
+                query = f"""
+                    UPDATE mining_news
+                    SET
+                        mining_source_id = %(mining_source_id)s,
+                        code = %(code)s,
+                        data = %(data)s,
+
+                        updated_at = now()
+                    WHERE
+                        code = %(code)s
+                    RETURNING *
+                """
+                cursor.execute(query, ({
+                    'mining_source_id': mining_source_id,
+                    'code': code,
+                    'data': data,
+                }))
+                connection.commit()
+                result = cursor.fetchone()
+
+        return result
+
+    @staticmethod
     def auto_update(
             mining_source_id=None,
             code=None,
