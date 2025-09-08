821d145a08a6aa53da5817c7ec780f4547eb5719
palindungan <rizkika.palindungan1@gmail.com>
2025-09-08 23:46:28 +0700

    Update mining_news_history.py

--- scripts/repositories/mining_news_history.py
+++ scripts/repositories/mining_news_history.py
@@ -5,4 +5,48 @@ class MiningNewsHistory:
             code=None,
             data=None,
     ):
-        pass
+        import os
+        import json
+        import psycopg2
+        from psycopg2 import extras
+        from dotenv import load_dotenv
+
+        load_dotenv()
+        db_url = os.getenv("DATABASE_URL")
+        connection = psycopg2.connect(db_url)
+
+        if data:
+            data = json.dumps(data)
+
+        with connection:
+            with connection.cursor(cursor_factory=extras.DictCursor) as cursor:
+                query = f"""
+                    INSERT INTO mining_news_histories
+                        (
+                            mining_source_id,
+                            code,
+                            data,
+
+                            created_at,
+                            updated_at
+                        )
+                    VALUES
+                        (
+                            %(mining_source_id)s,
+                            %(code)s,
+                            %(data)s,
+
+                            now(),
+                            now()
+                        )
+                    RETURNING *
+                """
+                cursor.execute(query, ({
+                    'mining_source_id': mining_source_id,
+                    'code': code,
+                    'data': data,
+                }))
+                connection.commit()
+                result = cursor.fetchone()
+
+        return result
