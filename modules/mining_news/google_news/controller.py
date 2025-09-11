a9e0aa99924e272319e4a1b6c1454d84a63bd378
palindungan <rizkika.palindungan1@gmail.com>
2025-09-11 21:34:52 +0700

    Update controller.py

--- modules/mining_news/google_news/controller.py
+++ modules/mining_news/google_news/controller.py
@@ -1,6 +1,19 @@
+from GoogleNews import GoogleNews
+
+
 class Controller:
     @staticmethod
     def mining(
             query=None,
     ):
-        pass
+        googlenews = GoogleNews()
+        googlenews.get_news(query)
+
+        page_current = 1
+
+        while True:
+            results = googlenews.page_at(page_current)
+            if results:
+                page_current += 1
+            else:
+                break
