e504b047423cf4f8cef0fa85278d03cc7d69b5be
palindungan <rizkika.palindungan1@gmail.com>
2025-09-11 20:49:51 +0700

    Update controller.py

--- modules/mining_news/google_search/controller.py
+++ modules/mining_news/google_search/controller.py
@@ -91,7 +91,7 @@ class Controller:
                 try:
                     next_button = driver.find_element(By.CSS_SELECTOR, f'a[aria-label="Page {page_current}"]')
                 except Exception as e:
-                    print(f"Exception: {e}")
+                    print(f"Exception: next_button {e}")
 
                 if next_button:
                     WebDriverWait(driver, 10).until(ec.element_to_be_clickable(next_button))
