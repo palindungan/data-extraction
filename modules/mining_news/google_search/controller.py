73eabf3437d90192f7948481fc99a31eb167867c
palindungan <rizkika.palindungan1@gmail.com>
2025-09-12 12:15:29 +0700

    Update controller.py

--- modules/mining_news/google_search/controller.py
+++ modules/mining_news/google_search/controller.py
@@ -23,28 +23,21 @@ class Controller:
         items = soup.find_all('div', {'class': 'N54PNb BToiNc'})
         for idx, item in enumerate(items):
             row_1 = item.find('div', {'class': 'kb0PBd A9Y9g jGGQ5e', 'data-snhf': '0'})
-            site_name = row_1.find('span', {'class': 'VuuXrf'}).text.strip()
+            publisher_title = row_1.find('span', {'class': 'VuuXrf'}).text.strip()
 
-            site_base_url = None
+            publisher_href = None
             if row_1.find('cite', {'class': 'qLRx3b tjvcx GvPZzd cHaqb'}):
-                site_base_url = row_1.find('cite', {'class': 'qLRx3b tjvcx GvPZzd cHaqb'}).contents[0].text.strip()
+                publisher_href = row_1.find('cite', {'class': 'qLRx3b tjvcx GvPZzd cHaqb'}).contents[0].text.strip()
 
             title = row_1.find('h3', {'class': 'LC20lb MBeuO DKV0Md'}).text.strip()
             url = row_1.find('a', {'class': 'zReHs'})['href']
 
             row_2 = item.find('div', {'class': 'kb0PBd A9Y9g', 'data-sncf': '1'})
-            date = None
+            published_date = None
             description = row_2.find('div', {'class': 'VwiC3b yXK7lf p4wth r025kc Hdw6tb'}).text.strip()
             if " — " in description:
-                date = description.split(" — ")[0]
+                published_date = description.split(" — ")[0]
                 description = description.split(" — ")[1]
-
-            title = item['title']
-            description = item['description']
-            published_date = item['published date']
-            url = item['url']
-            publisher_href = item['publisher']['href']
-            publisher_title = item['publisher']['title']
 
             metadata = {
                 'title': Helpers.normalization_text(text=title),
