d12cb1c3220b0a619a9654e8e0956a88fb833be7
palindungan <rizkika.palindungan1@gmail.com>
2025-09-12 13:41:13 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
 from modules.mining_news.google_news.controller import Controller as GoogleNewsController
 from modules.mining_news.google_search.controller import Controller as GoogleSearchController
 
-query = "yuri mansury"
+name = "yuri mansury"
 
-print("GoogleNewsController.mining")
-GoogleNewsController.mining(query=query)
-print("")
+queries = [
+    name,
 
-print("GoogleSearchController.mining")
-GoogleSearchController.mining(query=query)
-print("")
+    f"berita {name}",
+    f"{name} news",
+
+    f"seminar {name}",
+    f"{name} seminar",
+
+    f"pertemuan {name}",
+    f"{name} meeting",
+]
+
+for idx, item in enumerate(queries):
+    print("GoogleNewsController.mining")
+    GoogleNewsController.mining(query=item)
+    print("")
+
+    print("GoogleSearchController.mining")
+    GoogleSearchController.mining(query=item)
+    print("")
