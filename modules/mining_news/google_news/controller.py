7eb386e7f3654f7b1715bf2ee8dd6697c7298148
palindungan <rizkika.palindungan1@gmail.com>
2025-09-12 11:18:04 +0700

    Update controller.py

--- modules/mining_news/google_news/controller.py
+++ modules/mining_news/google_news/controller.py
@@ -2,6 +2,11 @@ import pprint
 
 from gnews import GNews
 
+from modules.mining_news.helpers import Helpers
+
+from scripts.repositories.mining_news import MiningNews as MiningNewsRepository
+from scripts.repositories.mining_news_history import MiningNewsHistory as MiningNewsHistoryRepository
+
 
 class Controller:
     @staticmethod
@@ -18,4 +23,28 @@ class Controller:
 
         for idx, item in enumerate(results):
             pprint.pp(item)
+
+            metadata = {
+                'site_name': Helpers.normalization_text(text=site_name),
+                'site_base_url': Helpers.normalization_text(text=site_base_url),
+
+                'title': Helpers.normalization_text(text=title),
+                'url': Helpers.normalization_text(text=url),
+
+                'date': Helpers.normalization_text(text=date),
+                'description': Helpers.normalization_text(text=description),
+            }
+            pprint.pp(metadata)
             print("--------------------------------------------------------------------------------")
+
+            MiningNewsHistoryRepository.store(
+                mining_source_id=2,
+                code=metadata['url'],
+                data=metadata,
+            )
+
+            MiningNewsRepository.auto_update(
+                mining_source_id=2,
+                code=metadata['url'],
+                data=metadata,
+            )
