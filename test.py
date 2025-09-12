a8a3b72caf00a9fdec824c57da2a3da7bba875f2
palindungan <rizkika.palindungan1@gmail.com>
2025-09-12 11:53:15 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from modules.mining_news.google_news.controller import Controller as GoogleNewsController
 from modules.mining_news.google_search.controller import Controller as GoogleSearchController
 
-query = "kasus presiden prabowo subianto"
+query = "yuri mansury"
 
 print("GoogleNewsController.mining")
 GoogleNewsController.mining(query=query)
